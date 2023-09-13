b78380cda8f71c7c9c783b3d7aeb3773212de2d3
Rich Compton <rich.compton@charter.com>
2023-09-13 16:24:18 -0600

    Update createAutoRenew_ARIN_RPKI_ROAs.py
    the maxLength field was not included on line 78

--- createAutoRenew_ARIN_RPKI_ROAs.py
+++ createAutoRenew_ARIN_RPKI_ROAs.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 import sys
 import argparse
 import base64
 import csv
 import time
-from datetime import datetime,timedelta
+from datetime import datetime, date
+#from datetime import datetime,timedelta
 from bs4 import BeautifulSoup
 import xml.dom.minidom
 import requests
+from logger import myLogger
 from bs4 import BeautifulSoup
-
-# In[2]:
 
 
 if sys.version_info<(3,6,5):
@@ -23,23 +21,18 @@ if sys.version_info<(3,6,5):
     sys.exit(1)
 
 
-# In[3]:
-
-
 # Get command line arguments and parse them
-parser = argparse.ArgumentParser(description='This is a script that reads in a CSV and makes API calls to ARIN to request ROAs. The API endpoint used is the updated version (2/23) that does not require signing by a private key. Only an API key generated after 2/23 will work.')
+parser = argparse.ArgumentParser(description='This is a script that reads in a CSV and makes API calls to ARIN to request ROAs.')
 parser.add_argument('-c','--csv', help='Specify a CSV with each line in the format: Origin AS,AS name,IP prefix,CIDR mask,maxLength.  Ex: 65000,Florida_Market,192.0.2.0,24,24',required=True)
-parser.add_argument('-a','--apikey', help='Specify the ARIN API key. Alternatively the API key can be put in a file called apikey.txt',required=False)
+parser.add_argument('-a','--apikey', help='Specify the ARIN API key.',required=False)
 parser.add_argument('-o','--orgid', help='Specify the ARIN ORG-ID associated with the prefixes.',required=True)
 parser.add_argument('-p','--production', action='store_true', help='Specify that you want to execute the API call in production and not OT&E.',required=False)
 parser.add_argument('--debug', action='store_true', help='Enable debug mode',required=False)
 
 
-# In[4]:
-
-
 # Parse all the arguments
 args = parser.parse_args()
+
 nowDate = datetime.now().strftime('%Y%m%d')
 
 if not args.apikey:
@@ -47,11 +40,9 @@ if not args.apikey:
        with open('apikey.txt') as key:
           args.apikey = key.read()[:-1]
     except:
+       myLogger.debug('Cannot open apikey.txt. please use -a')
        print('Cannot open apikey.txt. please use -a')
        exit()
-
-
-# In[ ]:
 
 
 # Does the user want to execute this on the production API?
@@ -68,16 +59,14 @@ if args.production:
         exit()
 
 
-# In[ ]:
-
 
 # Define a function to generate the roaData
-def generate_roaData(asn: str, asn_name: str, prefix: str, mask: str, maxLength: str) -> str:
+def generate_roaData(asn: str, asn_name: str, prefix: str, mask: str, maxLengh: str) -> str:
     # add roaSpec
     if asn_name:
         name = asn_name.replace('::', '-').replace(':','-').replace('.','-')
     else:
-        name = 'AS' + asn + '-NET-' + prefix.replace('::', '-').replace(':','-').replace('.','-') + '-' + mask + '-' + maxLength + '-' + nowDate
+        name = 'AS' + asn + '-NET-' + prefix.replace('::', '-').replace(':','-').replace('.','-') + '-' + mask + '-' + maxLengh + '-' + nowDate
     roaSpec = f'''
         <roaSpec> 
             <asNumber>{asn}</asNumber> 
@@ -85,7 +74,8 @@ def generate_roaData(asn: str, asn_name: str, prefix: str, mask: str, maxLength:
                 <resources> 
                     <roaSpecResource> 
                         <startAddress>{prefix}</startAddress> 
-                        <cidrLength>{mask}</cidrLength> 
+                        <cidrLength>{mask}</cidrLength>
+                        <maxLength>{maxLength}</maxLength>
                     </roaSpecResource> 
                 </resources> 
         </roaSpec>'''
@@ -93,12 +83,8 @@ def generate_roaData(asn: str, asn_name: str, prefix: str, mask: str, maxLength:
     return roaSpec
 
 
-
-# In[ ]:
-
-
 # Perform the POST to the ARIN API to request the ROA and get the return code
-def roa_request(roaData: str) -> str:
+def roa_request(roaData: str, log) -> str:
     '''
     Builds the ARIN ROA object per API details
     https://www.arin.net/resources/manage/regrws/payloads/#rpki-transaction-payload
@@ -113,8 +99,10 @@ def roa_request(roaData: str) -> str:
     else:
         host = 'reg.ote.arin.net'
 
-    payload = f'''<rpkiTransaction xmlns="http://www.arin.net/regrws/rpki/v1"><roaSpecAdd>{roaData}</roaSpecAdd></rpkiTransaction>'''
-
+    # Construct the XML data
+    payload = f'''<rpkiTransaction xmlns="http://www.arin.net/regrws/rpki/v1">
+                    <roaSpecAdd>{roaData}</roaSpecAdd>
+                </rpkiTransaction>'''
     # Construct the headers
     headers = {'Content-Type': 'application/xml', 'Accept': 'application/xml'}
     # Construct the URL
@@ -122,7 +110,7 @@ def roa_request(roaData: str) -> str:
     if args.debug:
         print(f'The URL to POST to is:\n{url}')
         print(f'The headers to POST are:\n{headers}')
-        print(f'The payload to post is:\n{type(payload)},{payload}')
+        print(f'The payload to post is:\n{payload}')
 
     # POST to API or error out
     try:
@@ -130,15 +118,17 @@ def roa_request(roaData: str) -> str:
         response = requests.post(url, data=payload, headers=headers)
     # Handle errors
     except requests.exceptions.HTTPError as e:
+        myLogger.debug('Uh oh we got an http error!')
         if args.debug:
             print('Uh oh we got an http error!')
         raise SystemExit(e)
     except requests.exceptions as e:
+        myLogger.debug('Uh oh we got a requests error!')
         if args.debug:
             print('Uh oh we got a requests error!')
         raise SystemExit(e)
     except Exception as e:
-        print('Uh oh we got some errors!')
+        myLogger.debug('Uh oh something wrong. error!')
 ###     ERROR
 
     dom = xml.dom.minidom.parseString(response.content.decode('utf-8'))
@@ -146,38 +136,42 @@ def roa_request(roaData: str) -> str:
 
     # Check to see if the response was 200.  If so, everything is ok.  Else print error response
     if str(response) == "<Response [200]>":
+        log.write(f'ROA successfully created for ROA: {roaData}\n')
         print(f'ROA successfully created for ROA: {roaData}')
     else:
-        #bs_data = BeautifulSoup(response.content.decode('utf-8'), 'xml')
-        bs_data = BeautifulSoup(response.content.decode('utf-8'), features='xml')
+        bs_data = BeautifulSoup(response.content.decode('utf-8'), 'xml')
         for idx,data in enumerate(bs_data.find_all('component')):
             message = data.find('message').text
-            print(f'ERROR! ROA creation failed for ROA: {str(response)} {roaData}! {message}')
+            log.write(f'ERROR! ROA creation failed for ROA: {roaData}! {message}\n')
+            print(f'ERROR! ROA creation failed for ROA: {roaData}! {message}')
 
 
     #Print debug
+    myLogger.debug(f'Response is:\n{response_content}')
     if args.debug:
         print(f'Response is:\n{response_content}')
 
     return response  
 
 
-# In[ ]:
-
-
 # Open the csv file and read each line
 with open(args.csv) as csvfile:
+    # log file
+    now = time.strftime('%m-%d_%H')
+    logName = '/tmp/RPKI/log/' + now + '_arin-roa-request.log'
     readCSV = csv.reader(csvfile, delimiter=',')
 
-    for row in readCSV:
-        body=''
+    with open(logName, "w+") as log:
+      for row in readCSV:
         # Set the various rows to the variables we need
+        body=''
         asn = row[0]
         asn_name = row[1]
         prefix = row[2]
         mask = row[3]
         maxLength = row[4]
-        # Generate the body for the roa request
         body = generate_roaData(asn, asn_name, prefix, mask, maxLength)
-        # Make the roa request
-        response_payload = roa_request(body)
+      #roaData_payload = createXml(body)
+      #response_payload = roa_request(roaData_payload, log)
+        response_payload = roa_request(body, log)
+
