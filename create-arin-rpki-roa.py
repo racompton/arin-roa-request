2af6db9b42ff5c4d26629d5f84126964778ce562
Kaleb Fenley <kalebfenley@gmail.com>
2024-07-29 16:56:02 -0500

    Refactor ARIN ROA request script - improve error handling - updated logging. - Fix typo in maxLength handling. - Add support for passing ARIN_API_KEY as an environment variable. - Update README with new filenames and script flags. - Update README with Table of Contents - added pip requirements file

--- create-arin-rpki-roa.py
+++ create-arin-rpki-roa.py
@@ -1,177 +1,157 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
 import sys
 import argparse
-import base64
 import csv
-import time
-from datetime import datetime, date
-#from datetime import datetime,timedelta
-from bs4 import BeautifulSoup
+from datetime import datetime
 import xml.dom.minidom
 import requests
-from logger import myLogger
-from bs4 import BeautifulSoup
+import logging
+import os
+from dotenv import load_dotenv
 
+# Initialize logging to write logs to a file and console
+log_filename = "arin-roa.log"
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+file_handler = logging.FileHandler(log_filename)
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(
+    logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+)
+logging.getLogger().addHandler(file_handler)
 
-if sys.version_info<(3,6,5):
-    sys.stderr.write('You need python 3.6.5+ or later to run this script!\n')
+# Check if running compatible python version
+if sys.version_info < (3, 6, 5):
+    logging.error("You need Python 3.6.5+ or later to run this script!")
     sys.exit(1)
 
-
 # Get command line arguments and parse them
-parser = argparse.ArgumentParser(description='This is a script that reads in a CSV and makes API calls to ARIN to request ROAs.')
-parser.add_argument('-c','--csv', help='Specify a CSV with each line in the format: Origin AS,AS name,IP prefix,CIDR mask,maxLength.  Ex: 65000,Florida_Market,192.0.2.0,24,24',required=True)
-parser.add_argument('-a','--apikey', help='Specify the ARIN API key.',required=False)
-parser.add_argument('-o','--orgid', help='Specify the ARIN ORG-ID associated with the prefixes.',required=True)
-parser.add_argument('-p','--production', action='store_true', help='Specify that you want to execute the API call in production and not OT&E.',required=False)
-parser.add_argument('--debug', action='store_true', help='Enable debug mode',required=False)
-
-
-# Parse all the arguments
+parser = argparse.ArgumentParser(description="Script to request ROAs via ARIN API.")
+parser.add_argument("-c", "--csv", help="Specify the CSV file.", required=True)
+parser.add_argument("-o", "--orgid", help="Specify the ARIN ORG-ID.", required=True)
+parser.add_argument(
+    "-p",
+    "--production",
+    action="store_true",
+    help="Use production API.",
+    required=False,
+)
+parser.add_argument(
+    "--debug", action="store_true", help="Enable debug mode.", required=False
+)
 args = parser.parse_args()
 
-nowDate = datetime.now().strftime('%Y%m%d')
+# Take environment variables from .env file
+load_dotenv()
+args.apikey = os.getenv("ARIN_API_KEY")
+
+nowDate = datetime.now().strftime("%Y%m%d")
 
 if not args.apikey:
-    try:
-       with open('apikey.txt') as key:
-          args.apikey = key.read()[:-1]
-    except:
-       myLogger.debug('Cannot open apikey.txt. please use -a')
-       print('Cannot open apikey.txt. please use -a')
-       exit()
+    logging.error(
+        "Please set ARIN_API_KEY as an environment variable or store it in .env file."
+    )
+    sys.exit(1)
 
-
-# Does the user want to execute this on the production API?
+# Triple check if user really wants to use production API
 if args.production:
-    print ('If you want to execute this on the Production API, please type \'production\'. Otherwise the application will exit.')
-    prod = str(input())
+    logging.info(
+        'To execute this on the Production API, type "production". Otherwise, the application will exit.'
+    )
+    prod = input().strip().lower()
 
-    if prod.lower() == 'production':
-        print('\nOK, just checking one more time that you REALLY want to execute this on the production API.  If so, type \'Yes\'. Otherwise the application will exit.')
-        yes_really_prod = str(input())
-        if yes_really_prod.lower() != 'yes':
-            exit()
-    else:
-        exit()
+    if prod != "production":
+        sys.exit(1)
+
+    logging.info('Please confirm by typing "yes".')
+    yes_really_prod = input().strip().lower()
+
+    if yes_really_prod != "yes":
+        sys.exit(1)
 
 
-
-# Define a function to generate the roaData
-def generate_roaData(asn: str, asn_name: str, prefix: str, mask: str, maxLengh: str) -> str:
-    # add roaSpec
-    if asn_name:
-        name = asn_name.replace('::', '-').replace(':','-').replace('.','-')
-    else:
-        name = 'AS' + asn + '-NET-' + prefix.replace('::', '-').replace(':','-').replace('.','-') + '-' + mask + '-' + maxLengh + '-' + nowDate
-    roaSpec = f'''
+# Function to generate the roaData
+def generate_roaData(asn: str, prefix: str, mask: str, maxLength: str) -> str:
+    name = f'AS{asn}-NET-{prefix.replace("::", "-").replace(":", "-").replace(".", "-")}-{mask}-{maxLength}-{nowDate}'
+    roaSpec = f"""
         <roaSpec> 
             <asNumber>{asn}</asNumber> 
             <name>{name}</name> 
-                <resources> 
-                    <roaSpecResource> 
-                        <startAddress>{prefix}</startAddress> 
-                        <cidrLength>{mask}</cidrLength>
-                        <maxLength>{maxLength}</maxLength>
-                    </roaSpecResource> 
-                </resources> 
-        </roaSpec>'''
-        
+            <resources> 
+                <roaSpecResource> 
+                    <startAddress>{prefix}</startAddress> 
+                    <cidrLength>{mask}</cidrLength>
+                    <maxLength>{maxLength}</maxLength>
+                </roaSpecResource> 
+            </resources> 
+        </roaSpec>"""
     return roaSpec
 
 
-# Perform the POST to the ARIN API to request the ROA and get the return code
-def roa_request(roaData: str, log) -> str:
-    '''
-    Builds the ARIN ROA object per API details
+# Function to perform the POST to the ARIN API
+def roa_request(roaData: str) -> str:
+    """
+    Post the ROA object to ARIN API
     https://www.arin.net/resources/manage/regrws/payloads/#rpki-transaction-payload
     https://www.arin.net/resources/manage/regrws/methods/#route-origin-authorizations-roas
     https://www.arin.net/reference/tools/testing/#using-reg-rws-and-the-irr-restful-api-in-ot-e
     https://www.arin.net/reference/tools/testing/#using-rpki-in-arin-s-ot-e-environment
-    '''
-    # Make API call to either prod or OT&E
-    if 'yes_really_prod' in globals():
-        if yes_really_prod.lower() == 'yes':
-            host = 'reg.arin.net'
-    else:
-        host = 'reg.ote.arin.net'
+    """
+    # Set API baseurl based off production or not
+    base_url = "reg.arin.net" if args.production else "reg.ote.arin.net"
 
-    # Construct the XML data
-    payload = f'''<rpkiTransaction xmlns="http://www.arin.net/regrws/rpki/v1">
+    payload = f"""<rpkiTransaction xmlns="http://www.arin.net/regrws/rpki/v1">
                     <roaSpecAdd>{roaData}</roaSpecAdd>
-                </rpkiTransaction>'''
+                </rpkiTransaction>"""
+
     # Construct the headers
-    headers = {'Content-Type': 'application/xml', 'Accept': 'application/xml'}
+    headers = {"Content-Type": "application/xml", "Accept": "application/xml"}
+
     # Construct the URL
-    url = f'https://{host}/rest/rpki/{args.orgid}?apikey={args.apikey}'
+    url = f"https://{base_url}/rest/rpki/{args.orgid}?apikey={args.apikey}"
+
     if args.debug:
-        print(f'The URL to POST to is:\n{url}')
-        print(f'The headers to POST are:\n{headers}')
-        print(f'The payload to post is:\n{payload}')
+        logging.debug(f"URL: {url}")
+        logging.debug(f"Headers: {headers}")
+        logging.debug(f"Payload: {payload}")
 
     # POST to API or error out
     try:
         # Make API call and get response
         response = requests.post(url, data=payload, headers=headers)
+        response.raise_for_status()
     # Handle errors
-    except requests.exceptions.HTTPError as e:
-        myLogger.debug('Uh oh we got an http error!')
-        if args.debug:
-            print('Uh oh we got an http error!')
-        raise SystemExit(e)
-    except requests.exceptions as e:
-        myLogger.debug('Uh oh we got a requests error!')
-        if args.debug:
-            print('Uh oh we got a requests error!')
-        raise SystemExit(e)
-    except Exception as e:
-        myLogger.debug('Uh oh something wrong. error!')
-###     ERROR
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Request failed: {e}")
+        sys.exit(1)
 
-    dom = xml.dom.minidom.parseString(response.content.decode('utf-8'))
+    # Parse response
+    dom = xml.dom.minidom.parseString(response.content.decode("utf-8"))
     response_content = dom.toprettyxml()
 
-    # Check to see if the response was 200.  If so, everything is ok.  Else print error response
-    if str(response) == "<Response [200]>":
-        log.write(f'ROA successfully created for ROA: {roaData}\n')
-        print(f'ROA successfully created for ROA: {roaData}')
+    # Check response status and handle errors
+    if response.status_code == 200:
+        logging.info(f"ROA successfully created: {roaData}")
     else:
-        bs_data = BeautifulSoup(response.content.decode('utf-8'), 'xml')
-        for idx,data in enumerate(bs_data.find_all('component')):
-            message = data.find('message').text
-            log.write(f'ERROR! ROA creation failed for ROA: {roaData}! {message}\n')
-            print(f'ERROR! ROA creation failed for ROA: {roaData}! {message}')
+        dom = xml.dom.minidom.parseString(response.content.decode("utf-8"))
+        errors = dom.getElementsByTagName("message")
+        for error in errors:
+            logging.error(
+                f"Error creating ROA: {roaData}, Message: {error.firstChild.nodeValue}"
+            )
+    # Log result
+    logging.debug(f"Response: {response_content}")
+    return response
 
 
-    #Print debug
-    myLogger.debug(f'Response is:\n{response_content}')
-    if args.debug:
-        print(f'Response is:\n{response_content}')
-
-    return response  
-
-
-# Open the csv file and read each line
+# Read and process the CSV file
 with open(args.csv) as csvfile:
-    # log file
-    now = time.strftime('%m-%d_%H')
-    logName = '/tmp/RPKI/log/' + now + '_arin-roa-request.log'
-    readCSV = csv.reader(csvfile, delimiter=',')
-
-    with open(logName, "w+") as log:
-      for row in readCSV:
-        # Set the various rows to the variables we need
-        body=''
-        asn = row[0]
-        asn_name = row[1]
-        prefix = row[2]
-        mask = row[3]
-        maxLength = row[4]
-        body = generate_roaData(asn, asn_name, prefix, mask, maxLength)
-      #roaData_payload = createXml(body)
-      #response_payload = roa_request(roaData_payload, log)
-        response_payload = roa_request(body, log)
-
+    readCSV = csv.reader(csvfile, delimiter=",")
+    with open(log_filename, "a") as log:
+        for row in readCSV:
+            asn, prefix, mask, maxLength = row
+            body = generate_roaData(asn, prefix, mask, maxLength)
+            response_payload = roa_request(body)
